eddeac89a9a6844e499f0ca2c333f43d411745c5
wisara88 <srandall845@gmail.com>
2020-09-28 22:34:22 -0700

    content ideas

---
+++ lib/content/list-content.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ContentList {
+  List lark = [
+    {'title': "upside down dance", 'points': 100},
+    {'title': "Art Attack", 'points': 100},
+    {'title': "write a poem about your driver", 'points': 100},
+  ];
+}
