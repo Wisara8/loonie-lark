363f02f1aaa3262e0c367df4e238c5b4574ea530
wisara88 <srandall845@gmail.com>
2020-09-30 01:12:03 -0700

    layout changes

--- lib/screens/loonie-list.dart
+++ lib/screens/loonie-list.dart
@@ -21,20 +21,85 @@ class _LoonieListState extends State<LoonieList> {
             child: Container(
               child: ListView(
                 children: <Widget>[
-                  ListTile(
-                    leading: Icon(
-                      Icons.photo_camera,
-                      color: Colors.white,
+                  Card(
+                    color: Colors.lightGreen,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.photo_camera,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        "Art Attack",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      subtitle: Text(
+                          "Make a large 6x6 art piece using creative mediums",
+                          style: TextStyle(color: Colors.white)),
+                      trailing: Text("100 points"),
+                      onTap: () {
+                        //image picker
+                        //save photo?
+                        //send data
+                      },
+                      onLongPress: () {
+                        // toggle card colour and strike text.
+                        // update score
+                      },
+                      isThreeLine: true,
                     ),
-                    title: Text(
-                      "Art Attack",
-                      style: TextStyle(color: Colors.white),
+                  ),
+                  Card(
+                    color: Colors.lightGreen,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        "Write an poem about your driver",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      subtitle: Text(
+                          "Gary drives a car, and he drives us far... ",
+                          style: TextStyle(color: Colors.white)),
+                      trailing: Text("75 points"),
+                      onTap: () {
+                        //image picker
+                        //save photo?
+                        //send data
+                      },
+                      onLongPress: () {
+                        // toggle card colour and strike text.
+                        // update score
+                      },
+                      isThreeLine: true,
                     ),
-                    subtitle: Text(
-                        "Make a large 6x6 art piece using creative mediums",
-                        style: TextStyle(color: Colors.white)),
-                    trailing: Text("100 points"),
-                    isThreeLine: true,
+                  ),
+                  Card(
+                    color: Colors.lightGreen,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.photo_camera,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        "Climb a tree",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      subtitle: Text("must be at least 5 feet off the ground",
+                          style: TextStyle(color: Colors.white)),
+                      trailing: Text("50 points"),
+                      onTap: () {
+                        //image picker
+                        //save photo?
+                        //send data
+                      },
+                      onLongPress: () {
+                        // toggle card colour and strike text.
+                        // update score
+                      },
+                      isThreeLine: true,
+                    ),
                   ),
                 ],
               ),
