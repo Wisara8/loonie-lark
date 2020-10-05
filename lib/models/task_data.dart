30d1b4f7b80acd5899c5119a49a6b5788c1be9c9
wisara88 <srandall845@gmail.com>
2020-10-05 01:51:20 -0700

    testing list scroll

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -10,7 +10,6 @@ class TaskData extends ChangeNotifier {
   }
 
   void toggleDone(Task task) {
-    print('in TaskData');
     task.toggleDone();
     notifyListeners();
   }
@@ -32,6 +31,36 @@ class TaskData extends ChangeNotifier {
       icon: Icons.photo_camera,
       title: "Art Attack",
       subTitle: '6x6 image made of small to medium items',
+      points: 100,
+    ),
+    Task(
+      icon: Icons.photo_camera,
+      title: "Write a poem about your driver",
+      subTitle: 'Make Robie Burns proud, no pressure',
+      points: 100,
+    ),
+    Task(
+      icon: Icons.photo_camera,
+      title: "Make a crown and wear it",
+      subTitle: 'In every picture for the rest of the night',
+      points: 100,
+    ),
+    Task(
+      icon: Icons.photo_camera,
+      title: "Whole team climbing a tree",
+      subTitle: 'If you break the tree -50000 points',
+      points: 100,
+    ),
+    Task(
+      icon: Icons.photo_camera,
+      title: "Play bumber cars with grocery buggies",
+      subTitle: 'At least one person should be hurt doing this',
+      points: 100,
+    ),
+    Task(
+      icon: Icons.photo_camera,
+      title: "Drink a beer upside down",
+      subTitle: 'Shotgun!',
       points: 100,
     ),
   ];
