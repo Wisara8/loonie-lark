3dae12d9e9dd49b3eefc9011d378c37478e757df
wisara88 <srandall845@gmail.com>
2021-05-21 23:51:21 -0700

    remaking a widget

--- lib/screens/placeholder.dart
+++ lib/screens/placeholder.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'task_tile.dart';
+import 'package:provider/provider.dart';
+import '../models/task_data.dart';
 
-class NewWidget extends StatelessWidget {
+class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              icon: taskData.tasks[index].icon,
+              title: taskData.tasks[index].title,
+              subTitle: taskData.tasks[index].subTitle,
+              points: taskData.tasks[index].points,
+              isDone: taskData.tasks[index].isDone,
+              toggleComplete: () {
+                taskData.toggleDone(taskData.tasks[index]);
+              },
+            );
+          },
+          itemCount: taskData.tasks.length,
+        );
+      },
+    );
   }
 }
