f03e1aee8af11b2dec5d49271fc589d9605f6c34
wisara88 <srandall845@gmail.com>
2021-05-22 10:50:55 -0700

    quick fix to placeholder widget

--- lib/screens/placeholder.dart
+++ lib/screens/placeholder.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'task_tile.dart';
 import 'package:provider/provider.dart';
 import '../models/task_data.dart';
+// import '../widgetyouwanttoimport';
 
-class TaskList extends StatelessWidget {
+class PlaceholderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
-            return TaskTile(
+            return importedWidget(
               icon: taskData.tasks[index].icon,
               title: taskData.tasks[index].title,
               subTitle: taskData.tasks[index].subTitle,
